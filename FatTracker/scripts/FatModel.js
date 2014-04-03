3a890e4f74b32722595e386f5194a2764eb7a100
Juergen Toth <juergen.toth@gmx.de>
2014-04-03 15:05:57 -0500

    Changes made by Juergen Toth (juergen.toth@gmx.de).

--- FatTracker/scripts/FatModel.js
+++ FatTracker/scripts/FatModel.js
@@ -1,51 +1,23 @@
 var fatViewModel = kendo.observable({
-    source: [ "foo", "bar" ],
-    newItemValue: "",
-    add: function(e) {
-        var val = this.get("newItemValue");
-        if (val) {
-            this.source.push(val);
-        }
-
-        $("#listview-new-item").blur();
-        e.preventDefault();
-    },
-
-    onClick: function(e) {
-        kendoConsole.log("event :: click (" + e.item.text() + ")", false, "#mobile-listview-events");
-    }
-});
-
-var viewModel = kendo.observable({
-
-    // the expenses array will hold the grid values
-    expenses: [],
-
-    // type array populates the drop down
-    type: [
-        { name: "Food", value: "food"}, 
-        { name: "Merchant", value: "merchant"}, 
-        { name: "Bills", value: "bills" }
-    ],
-
-    // expenseType holds the currently selected value of the dropdown list
-    expenseType: "food", 
-
-    // the values are bound to the merchant and amount fields
-    merchant: null,
-    amount: null,
-
-    // event to execute on click of add button
+    
+    entryDate: null,
+    fat: null,
+    water: null,
+    muscle: null,
+    
+    fats: [],
+ 
+    // add a new fat entry
     create: function(e) {
 
         // add the items to the array of expenses 
-        this.get("expenses").push({Type: this.get("expenseType"), 
-                                   Merchant: this.get("merchant"), 
-                                   Amount: this.get("amount")});
+        this.get("fats").push({EntryDate: this.get("entryDate"), 
+                                   Fat: this.get("fat"), 
+                                   Water: this.get("water"),
+             					  Muscle: this.get("muscle")});
 
-        // reset the form 
-        this.set("expenseType", "food");
-        this.set("merchant", "");
-        this.set("amount", "");
+ $("#txtFat").blur();
+            e.preventDefault();
     }
+    
 });
