4003a48209647bf9141c4140356c97e10418f5a6
Juergen Toth <juergen.toth@gmx.de>
2014-04-03 06:34:35 -0500

    Changes made by Juergen Toth (juergen.toth@gmx.de).

--- FatTracker/scripts/FatModel.js
+++ FatTracker/scripts/FatModel.js
@@ -15,3 +15,37 @@ var mobileListViewModel = kendo.observable({
         kendoConsole.log("event :: click (" + e.item.text() + ")", false, "#mobile-listview-events");
     }
 });
+
+var viewModel = kendo.observable({
+
+    // the expenses array will hold the grid values
+    expenses: [],
+
+    // type array populates the drop down
+    type: [
+        { name: "Food", value: "food"}, 
+        { name: "Merchant", value: "merchant"}, 
+        { name: "Bills", value: "bills" }
+    ],
+
+    // expenseType holds the currently selected value of the dropdown list
+    expenseType: "food", 
+
+    // the values are bound to the merchant and amount fields
+    merchant: null,
+    amount: null,
+
+    // event to execute on click of add button
+    create: function(e) {
+
+        // add the items to the array of expenses 
+        this.get("expenses").push({Type: this.get("expenseType"), 
+                                   Merchant: this.get("merchant"), 
+                                   Amount: this.get("amount")});
+
+        // reset the form 
+        this.set("expenseType", "food");
+        this.set("merchant", "");
+        this.set("amount", "");
+    }
+});
