6dc106caa910ac232bd71cf810da845800ef0658
Juergen Toth <juergen.toth@gmx.de>
2014-04-16 17:15:59 -0500

    Changes made by Juergen Toth (juergen.toth@gmx.de).

--- FatTracker/scripts/FatChart.js
+++ FatTracker/scripts/FatChart.js
@@ -1,30 +1,38 @@
 
 function drawChart(e) {
     
-    alert("Dataset count: " + fatViewModel.fats.length);
+
+    
+var allFats =_.pluck(fatViewModel.fats, 'Fat'), 
+    allWater = _.pluck(fatViewModel.fats, 'Water'), 
+    allMuscle = _.pluck(fatViewModel.fats, 'Muscle'), 
+    allDates = _.pluck(fatViewModel.fats, 'EntryDate');
+    
+    debugger;
+    alert(allWater.length);
     
 var lineChartData = {
-			labels : ["January","February","March","April","May","June","July"],
+			labels : allDates,
 			datasets : [
 				{
 					fillColor : "rgba(220,220,220,0.5)",
 					strokeColor : "rgba(220,220,220,1)",
 					pointColor : "rgba(220,220,220,1)",
 					pointStrokeColor : "#fff",
-					data : [65,59,90,81,56,55,40]
+					data : allFats
 				},
 				{
 					fillColor : "rgba(151,187,205,0.5)",
 					strokeColor : "rgba(151,187,205,1)",
 					pointColor : "rgba(151,187,205,1)",
 					pointStrokeColor : "#fff",
-					data : [28,48,40,19,96,27,100]
+					data : allWater
 				}
 			]
 
 		};
 
-    var myLine = new Chart(document.getElementById("fatCanvas").getContext("2d")).Line(lineChartData);
+    new Chart(document.getElementById("fatCanvas").getContext("2d")).Line(lineChartData);
 }
 
 	
